dba9f4dfc157232f547754ea3984f30fc137871e
topisteronyango <t.onyango@alustudent.com>
2023-11-28 17:56:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 import streamlit as st
-from werkzeug.utils import secure_filename
 import os
-
 import cv2
 import numpy as np
 from keras.models import load_model
 
-
 # Load the saved model
-# loaded_model = None  # Load your model here (ensure it's accessible)
 loaded_model = load_model('defect_detection_model.h5')
 
 # Define the path to the uploaded images folder
@@ -17,8 +13,7 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 # Function to detect defects
-def detect_defect(image_path):
-    img = cv2.imread(image_path)
+def detect_defect(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert to RGB
     img = cv2.resize(img, (224, 224))  # Resize image if needed
     img = img / 255.0  # Normalize pixel values
@@ -40,11 +35,14 @@ st.title('Defect Detection')
 uploaded_file = st.file_uploader("Upload an image", type=['png', 'jpg', 'jpeg', 'bmp'])
 
 if uploaded_file is not None:
+    img = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1)
     
-    file_location = os.path.join(UPLOAD_FOLDER, secure_filename(uploaded_file.name))
-    with open(file_location, "wb") as f:
-        f.write(uploaded_file.getbuffer())
+    if img is not None:
+        file_location = os.path.join(UPLOAD_FOLDER, uploaded_file.name)
+        cv2.imwrite(file_location, cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
 
-    detection_result = detect_defect(file_location)
-    st.write("Detection Result:", detection_result)
-    st.image(file_location, caption='Uploaded Image', use_column_width=True)
+        detection_result = detect_defect(img)
+        st.write("Detection Result:", detection_result)
+        st.image(file_location, caption='Uploaded Image', use_column_width=True)
+    else:
+        st.write("Invalid Image File. Please upload a valid image.")
